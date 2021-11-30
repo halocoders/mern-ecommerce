b22f3ee2ad669eb6f018634105deceae42922b95
Prio Pambudi <priopambudi869@gmail.com>
2021-11-30 15:47:21 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+
+function App() {
+  return (
+    <div>
+      {/* <Home /> */}
+      {/* <ProductList /> */}
+      <Product />
+    </div>
+  );
+}
+
+export default App;
