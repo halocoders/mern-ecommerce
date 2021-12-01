c747f6af27b58c81235a4b1b08a179dd870d1ff9
Prio Pambudi <priopambudi869@gmail.com>
2021-12-01 12:49:13 +0700

    backend auth

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const authRouter = require('./routes/auth');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('DBConnection Success');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Backend is running!');
+});
