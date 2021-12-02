c4f04c3917b3e4f8f23339232ac559e33d766cca
Prio Pambudi <priopambudi869@gmail.com>
2021-12-02 21:28:27 +0700

    stripe

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const cors = require('cors');
 
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
 const productRouter = require('./routes/product');
 const cartRouter = require('./routes/cart');
 const orderRouter = require('./routes/order');
+const stripeRouter = require('./routes/stripe');
 
 dotenv.config();
 
@@ -20,12 +22,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/checkout', stripeRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend is running!');
